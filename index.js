7540d34f5d25b4cd5110a5aa8fa1f33a7dee7694
ArmstrongVD <Dimitri.Armstrong@va.gov>
2024-11-25 09:25:29 -0500

    Added debounce function to help with reduction to  API Calls while typing in the search bar

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Hi there!');
+let myAPIKey = "http://www.omdbapi.com/";
+document.title = "Movies";
+
+const fetchData = async (searchTerm) => {
+  const response = await axios.get(myAPIKey, {
+    params: {
+      apikey: "93ae98ca",
+      s: searchTerm,
+    },
+  });
+};
+
+const input = document.querySelector("input");
+
+const debounce = (func, delay = 1000) => {
+  let timeOutId;
+  return (...args) => {
+    if (timeOutId) {
+      clearTimeout(timeOutId);
+    }
+    timeOutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+};
+
+const onInput = (event) => {
+  fetchData(event.target.value);
+};
+input.addEventListener("input", debounce(onInput, 500));
